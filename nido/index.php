469004aca03082b90c8e2612d6c59b2285dfff2a
mirzasaadbaig99 <mirzasaadbaigmughal@gmail.com>
2019-01-19 14:41:44 -0800

    initialize our project

---
+++ nido/index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My First Web</title>
+<style type="text/css">
+	.heading-style
+	{color: red;
+		font-style: verdana;
+		font-size: 50px;
+
+
+
+	}
+	#heading-style
+	{color: cyan;
+		font-style: verdana;
+		font-size: 50px;
+
+
+
+	}
+
+
+</style>
+
+
+
+
+</head>
+<body>
+<h1 style="color: purple">Welcome</h1>
+<h2 class="heading-style">Welcome</h2>
+<h3 id="heading-style">Welcome</h3>
+<h4>Welcome</h4>
+<h5>Welcome</h5>
+<h6>Welcome</h6>
+
+<p>This is paragraph text</p>
+<b>This is Bold Text</b>
+<pre>This is space                       tag</pre>
+
+<ul><h3><u>Unorder List</u></h3></ul>
+<ul>
+<li>HTML</li>
+<li>CSS</li>
+<li>PHP</li>
+</ul>
+
+<ol><h3><u><i>Order List</u></i></u></h3></ol>
+<ol type="i">
+<li>HTML</li>
+<li>CSS</li>
+<li>PHP</li>
+
+</ol>
+
+<hl>Detailed List</hl>
+<dl>
+	<dt>Web Designing</dt>
+	<ol>
+	<li>HTML</li>
+	<li>CSS</li>
+	<li>PHP</li>
+	<li>MY SQL</li>
+</ol>
+
+<dt>Web Development</dt>
+	<ol>
+	<li>HTML</li>
+	<li>CSS</li>
+	<li>PHP</li>
+	<li>MY SQL</li>
+</ol>
+
+
+</dl>
+
+
+
+</body>
+</html>
